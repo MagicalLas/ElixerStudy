c6fcd7b5fcd56793dcc4e621bb6f4e7084ba0172
Las-Wonho <haho013039@gmail.com>
2018-07-25 12:40:59 +0900

    Other If, Else

---
+++ Source/09_Unless_is_UnIf.exs
@@ -0,0 +1,15 @@
+unless 1 == 1 do
+    IO.puts "1 is 1"
+else
+    IO.puts "1 is not 1"
+end 
+
+unless nil do
+    IO.puts "nil like to false"
+end
+
+if nil do
+    IO.puts "nil is true"
+else
+    IO.puts "nil is false"
+end
